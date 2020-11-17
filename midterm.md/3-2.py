c8106da8b0b106832040dc462ac90e62bec12cb0
HouChiaWei <jack0984343845@gmail.com>
2020-11-17 07:00:36 +0000

    update change

---
+++ midterm.md/3-2.py
@@ -0,0 +1,11 @@
+a=float(input())
+b=float(input())
+c=float(input())
+
+d=((b**2-4*a*c)**(1/2))
+
+X1=(-b+d)/(2*a)
+X2=(-b-d)/(2*a)
+
+print(X1)
+print(X2)
---
+++ midterm.md/3-3.py
@@ -0,0 +1,12 @@
+a=float(input())
+b=float(input())
+c=float(input())
+d=float(input())
+e=float(input())
+f=float(input())
+
+X=(b*f-c*e)/(a*e-b*d)
+Y=(a*f-d*c)/(b*d-a*e)
+
+print(X)
+print(Y)
