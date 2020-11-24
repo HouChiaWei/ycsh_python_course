13ee331f469771ef2ddcc54c8232029942b790b0
HouChiaWei <jack0984343845@gmail.com>
2020-11-24 02:08:21 +0000

    update change

--- L07/ttt_update_cell.py
+++ L07/ttt_update_cell.py
@@ -49,5 +49,7 @@ elif position == 9:
 # 顯示棋盤
 # TODO: 程式寫這裡
 print(' ' + cell_1 + ' | ' + cell_2 + ' | ' + cell_3 + ' ')
+print('---+---+---')
 print(' ' + cell_4 + ' | ' + cell_5 + ' | ' + cell_6 + ' ')
+print('---+---+---')
 print(' ' + cell_7 + ' | ' + cell_8 + ' | ' + cell_9 + ' ')
